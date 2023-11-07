d81936f4a3eac9b1980a36a63cd78e8ee9f5c00c
DikosAs <uraevdmitrij031@gmail.com>
2023-11-07 21:50:32 +0400

    Оптимизировал главное меню

--- UnitsMTW2.py
+++ UnitsMTW2.py
@@ -6,6 +6,9 @@ import json
 with open("./units.json", "r", encoding="UTF-8") as file:
     units_json = json.load(file)
 
+def getFractionsName() -> list[str]:
+    return [units_json.get("fractions")[id].get("fraction_name") for id in range(0, len(units_json.get("fractions")))]
+
 def App(page: flet.Page):
     page.title = "Units Medieval Total War 2"
     page.update()
@@ -14,14 +17,14 @@ def App(page: flet.Page):
         page.controls.clear()
         page.vertical_alignment = MainAxisAlignment.CENTER
 
-        fractions = units_json.get("fractions")
+        fractionsName = getFractionsName()
 
-        for id in range(0, len(fractions), 2):
+        for id in range(0, len(fractionsName), 2):
             page.add(
                 Row(
                     [
-                        TextButton(text=fractions[id].get("fraction_name"), width=200, height=50, on_click=...),
-                        TextButton(text=fractions[id + 1].get("fraction_name"), width=200, height=50, on_click=...)
+                        TextButton(text=fractionsName[id], width=200, height=50, on_click=unitTypySelectMenu(id)),
+                        TextButton(text=fractionsName[id+1], width=200, height=50, on_click=unitTypySelectMenu(id+1))
                     ],
                     alignment=MainAxisAlignment.CENTER
                 )
@@ -29,18 +32,19 @@ def App(page: flet.Page):
 
         page.update()
 
-    def unitTypySelectMenu(e, fraction):
+    def unitTypySelectMenu(fractionID: int):
         page.controls.clear()
         page.vertical_alignment = MainAxisAlignment.CENTER
 
-        fractions = units_json.get("fractions")
+        unitsTypes = list(units_json.get("fractions")[fractionID].get("units").keys())
+        print(unitsTypes)
 
-        for id in range(0, len(fractions), 2):
+        for id in range(0, len(unitsTypes), 2):
             page.add(
                 Row(
                     [
-                        TextButton(text=fractions[id].get("fraction_name"), width=200, height=50, on_click=...),
-                        TextButton(text=fractions[id + 1].get("fraction_name"), width=200, height=50, on_click=...)
+                        TextButton(text=unitsTypes[id].get("unitTypeName"), width=200, height=50, on_click=...),
+                        TextButton(text=unitsTypes[id+1].get("unitTypeName"), width=200, height=50, on_click=...)
                     ],
                     alignment=MainAxisAlignment.CENTER
                 )
